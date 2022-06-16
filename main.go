b0946a406d7665a60140f596b82164274da97f68
mirzaleka <mirza_376@hotmail.com>
2022-06-16 11:35:00 +0200

    Hello World

---
+++ main.go
@@ -0,0 +1,10 @@
+package main // executable package
+
+import "fmt" // format library
+
+func main() {
+  fmt.Println("Hello World!"); // console.log
+}
+
+// Run: go run main.go
+// Build executable: go build main.go
